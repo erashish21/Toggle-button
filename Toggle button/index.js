62b9554ef7efe5b2aba132288a597a60be362152
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2023-02-04 14:18:36 +0530

    Toggle upload

---
+++ Toggle button/index.js
@@ -0,0 +1,9 @@
+var app = document.getElementById("app");
+
+app.addEventListener("click", myFunction());
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  
+}
